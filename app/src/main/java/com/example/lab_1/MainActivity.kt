36c1b72361787322548da39190c67db736be21d1
Bulochka56 <bul.xajrullin@yandex.ru>
2023-10-03 19:25:04 +0300

    add animation

--- app/src/main/java/com/example/lab_1/MainActivity.kt
+++ app/src/main/java/com/example/lab_1/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.lab_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val imageView = findViewById<ImageView>(R.id.imageView)
+
+        // Загрузите анимацию из ресурсов
+        val slideInLeftAnimation = AnimationUtils.loadAnimation(this, R.anim.slide_in_left)
+
+        // Примените анимацию к изображению
+        imageView.startAnimation(slideInLeftAnimation)
     }
 }
